64f08752f4b1bc5a41d81a76f401842782f9a791
Johnnyup <>
2021-06-07 20:27:26 +0000

    1527

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const input = require('readline-sync');
 let num = input.question('Please enter a positive number:');
 num = Number(num);
 
-while (num <= 0) {
-   num = input.question('Invalid input. Please enter a positive number:');
-   num = Number(num);
+while (num >= 0) {
+   num = input.question('Affirmative. Please enter a positive number:');
+   num = Number(num); 
 }
+
